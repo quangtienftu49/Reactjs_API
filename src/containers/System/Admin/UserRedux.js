1bcd552ff7c3651936e5a389f8dd371e52cf1818
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-02-01 23:42:33 -0500

    add form creating users multi languages

---
+++ src/containers/System/Admin/UserRedux.js
@@ -0,0 +1,143 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import { getAllCodeService } from "../../../services/userService";
+import { LANGUAGES } from "../../../utils";
+
+class UserRedux extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      genderArr: [],
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      let res = await getAllCodeService("gender");
+      if (res && res.errCode === 0) {
+        this.setState({
+          genderArr: res.data,
+        });
+      }
+      console.log("check res redux form", res);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    let genders = this.state.genderArr;
+    let language = this.props.language;
+    return (
+      <div className="user-redux-container">
+        <div className="title">
+          <FormattedMessage id="manage-user.reduxTitle" />
+        </div>
+
+        <div className="user-redux-body">
+          <div className="container">
+            <div className="row">
+              <div className="col-12 my-3">
+                <FormattedMessage id="manage-user.add" />
+              </div>
+              <div className="col-3">
+                <label>Email</label>
+                <input type="email" className="form-control" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.password" />
+                </label>
+                <input type="password" className="form-control" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.firstName" />
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.lastName" />
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.phoneNumber" />
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-9">
+                <label>
+                  <FormattedMessage id="manage-user.address" />
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.gender" />
+                </label>
+                <select className="form-control">
+                  {genders &&
+                    genders.length > 0 &&
+                    genders.map((item, index) => {
+                      return (
+                        <option key={index}>
+                          {language === LANGUAGES.VI
+                            ? item.valueVi
+                            : item.valueEn}
+                        </option>
+                      );
+                    })}
+                </select>
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.title" />
+                </label>
+                <select className="form-control">
+                  <option selected>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.roleId" />
+                </label>
+                <select className="form-control">
+                  <option selected>Choose...</option>
+                  <option>...</option>
+                </select>
+              </div>
+              <div className="col-3">
+                <label>
+                  <FormattedMessage id="manage-user.image" />
+                </label>
+                <input type="text" className="form-control" />
+              </div>
+              <div className="col-12 mt-3">
+                <button className="btn btn-primary">
+                  <FormattedMessage id="manage-user.save" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserRedux);
