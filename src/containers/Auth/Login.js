bcf2a2f0234ca8547fb0bf43ea6d66e09afe8f79
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-02-09 01:19:09 -0500

    add page to pick doctor and appointment time, fix bug not login when hitting enter

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -1,142 +1,166 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { push } from "connected-react-router";
 import * as actions from "../../store/actions";
-import './Login.scss';
+import "./Login.scss";
 // import { FormattedMessage } from 'react-intl';
-import { handleLoginApi } from '../../services/userService';
-import { userLoginSuccess } from '../../store/actions';
+import { handleLoginApi } from "../../services/userService";
+import { userLoginSuccess } from "../../store/actions";
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
       isShowPassword: false,
-      errMessage: ''
-    }
+      errMessage: "",
+    };
   }
 
   handleOnChangeUsername = (e) => {
     this.setState({
-      username: e.target.value
-    })
-  }
+      username: e.target.value,
+    });
+  };
 
   handleOnChangePassword = (e) => {
     this.setState({
-      password: e.target.value
-    })
-  }
+      password: e.target.value,
+    });
+  };
 
   handleLogin = async () => {
     this.setState({
-      errMessage: ''
-    })
+      errMessage: "",
+    });
 
     try {
       let data = await handleLoginApi(this.state.username, this.state.password);
 
-      if(data && data.errCode !== 0) {
+      if (data && data.errCode !== 0) {
         this.setState({
-          errMessage: data.message
-        })
+          errMessage: data.message,
+        });
       }
 
-      if(data && data.errCode === 0){
-        this.props.userLoginSuccess(data.user)
+      if (data && data.errCode === 0) {
+        this.props.userLoginSuccess(data.user);
       }
-
     } catch (error) {
       if (error.response) {
         if (error.response.data) {
           this.setState({
-            errMessage: error.response.data.message
-          })
+            errMessage: error.response.data.message,
+          });
         }
       }
     }
-  }
+  };
 
   handleShowHidePassword = () => {
     this.setState({
-      isShowPassword: !this.state.isShowPassword
-    })
-  }
+      isShowPassword: !this.state.isShowPassword,
+    });
+  };
+
+  handleKeyDown = (e) => {
+    if (e.key === "Enter" || e.keyCode === 13) {
+      this.handleLogin();
+    }
+  };
 
   render() {
-
     return (
-      <div className='login-background'>
-        <div className='login-container'>
-          <div className='login-content row'>
-            <div className='col-12 login-text'>Login</div>
-            <div className='col-12 form-group login-input'>
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 login-text">Login</div>
+            <div className="col-12 form-group login-input">
               <label>Username</label>
               <input
-                type='text'
-                className='form-control'
+                type="text"
+                className="form-control"
                 placeholder="Enter your username"
                 value={this.state.username}
-                onChange={(e) => { this.handleOnChangeUsername(e) }}
+                onChange={(e) => {
+                  this.handleOnChangeUsername(e);
+                }}
               />
             </div>
-            <div className='col-12 form-group login-input'>
+            <div className="col-12 form-group login-input">
               <label>Password</label>
-              <div className='custom-input-password'>
+              <div className="custom-input-password">
                 <input
-                  type={this.state.isShowPassword ? 'text' : 'password'} className='form-control'
+                  type={this.state.isShowPassword ? "text" : "password"}
+                  className="form-control"
                   placeholder="Enter your password"
                   value={this.state.password}
-                  onChange={(e) => { this.handleOnChangePassword(e) }}
+                  onChange={(e) => {
+                    this.handleOnChangePassword(e);
+                  }}
+                  onKeyDown={(e) => {
+                    this.handleKeyDown(e);
+                  }}
                 />
 
                 <span
-                  onClick={() => { this.handleShowHidePassword() }}
-
+                  onClick={() => {
+                    this.handleShowHidePassword();
+                  }}
                 >
-                  <i className={this.state.isShowPassword ? "far fa-eye" : "far fa-eye-slash"} />
+                  <i
+                    className={
+                      this.state.isShowPassword
+                        ? "far fa-eye"
+                        : "far fa-eye-slash"
+                    }
+                  />
                 </span>
               </div>
             </div>
-            <div className='col-12' style={{ color: 'red' }} >
+            <div className="col-12" style={{ color: "red" }}>
               {this.state.errMessage}
             </div>
-            <div className='col-12'>
+            <div className="col-12">
               <button
-                className='btn-login'
-                onClick={() => { this.handleLogin() }}
-              >Login</button>
+                className="btn-login"
+                onClick={() => {
+                  this.handleLogin();
+                }}
+              >
+                Login
+              </button>
             </div>
-            <div className='col-12'>
-              <span className='forgot-password'>Forgot your password?</span>
+            <div className="col-12">
+              <span className="forgot-password">Forgot your password?</span>
             </div>
-            <div className='col-12 text-center mt-3'>
-              <span className='text-other-login'>Or login with</span>
+            <div className="col-12 text-center mt-3">
+              <span className="text-other-login">Or login with</span>
             </div>
-            <div className='col-12 social-login'>
+            <div className="col-12 social-login">
               <i className="fab fa-google-plus-g google"></i>
               <i className="fab fa-facebook-f facebook"></i>
             </div>
           </div>
         </div>
-      </div >
-    )
+      </div>
+    );
   }
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
-    lang: state.app.language
+    lang: state.app.language,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     navigate: (path) => dispatch(push(path)),
     // userLoginFail: () => dispatch(actions.userLoginFail()),
-    userLoginSuccess: (userInfor)=> dispatch(actions.userLoginSuccess(userInfor))
+    userLoginSuccess: (userInfor) =>
+      dispatch(actions.userLoginSuccess(userInfor)),
   };
 };
 
--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -1,11 +1,137 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./ManageSchedule.scss";
+import { FormattedDateParts, FormattedMessage } from "react-intl";
+import Select from "react-select";
+import * as actions from "../../../store/actions";
+import { CRUD_ACTIONS, LANGUAGES } from "../../../utils";
+import DatePicker from "../../../components/Input/DatePicker";
+import moment from "moment";
 
 class ManageSchedule extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listDoctors: [],
+      selectedDoctor: {},
+      currentDate: "",
+      rangeTime: [],
+    };
+  }
+
+  componentDidMount() {
+    this.props.fetchAllDoctors();
+    this.props.fetchAllScheduleTime();
+  }
+
+  buildDataInputSelect = (inputData) => {
+    let result = [];
+    let { language } = this.props;
+
+    if (inputData && inputData.length > 0) {
+      inputData.map((item, index) => {
+        let object = {};
+        let labelVi = `${item.lastName} ${item.firstName}`;
+        let labelEn = `${item.firstName} ${item.lastName}`;
+
+        object.label = language === LANGUAGES.VI ? labelVi : labelEn;
+        object.value = item.id;
+
+        result.push(object);
+      });
+    }
+
+    return result;
+  };
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.allDoctors !== this.props.allDoctors) {
+      let dataSelect = this.buildDataInputSelect(this.props.allDoctors);
+      this.setState({
+        listDoctors: dataSelect,
+      });
+    }
+
+    if (prevProps.allScheduleTime !== this.props.allScheduleTime) {
+      this.setState({
+        rangeTime: this.props.allScheduleTime,
+      });
+    }
+
+    // if (prevProps.language !== this.props.language) {
+    //   let dataSelect = this.buildDataInputSelect(this.props.allDoctors);
+    //   this.setState({
+    //     listDoctors: dataSelect,
+    //   });
+    // }
+  }
+
+  handleChangeSelect = async (selectedOption) => {
+    this.setState({ selectedDoctor: selectedOption });
+  };
+
+  handleOnChangeDatePicker = (date) => {
+    this.setState({
+      currentDate: date,
+    });
+  };
+
   render() {
+    console.log("check state", this.state);
+    let { rangeTime } = this.state;
+    let { language } = this.props;
     return (
       <>
-        <div>Manage schedule</div>
+        <div className="manage-schedule-container">
+          <div className="manage-schedule-title">
+            <FormattedMessage id="manage-schedule.title" />
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-6 form-group">
+                <label>
+                  <FormattedMessage id="manage-schedule.choose-doctor" />
+                </label>
+
+                <Select
+                  value={this.state.selectedDoctor}
+                  onChange={this.handleChangeSelect}
+                  options={this.state.listDoctors}
+                />
+              </div>
+              <div className="col-6 form-group">
+                <label>
+                  <FormattedMessage id="manage-schedule.choose-date" />
+                </label>
+                <DatePicker
+                  onChange={this.handleOnChangeDatePicker}
+                  className="form-control"
+                  value={this.state.currentDate[0]}
+                  minDate={new Date()}
+                />
+              </div>
+              <div className="col-12 pick-hour-container">
+                {rangeTime &&
+                  rangeTime.length > 0 &&
+                  rangeTime.map((item, index) => {
+                    return (
+                      <button key={index} className="btn btn-schedule">
+                        {language === LANGUAGES.VI
+                          ? item.valueVi
+                          : item.valueEn}
+                      </button>
+                    );
+                  })}
+              </div>
+              <div className="col-12">
+                <button className="btn btn-primary btn-save-schedule">
+                  <FormattedMessage id="manage-schedule.save-appointment" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
@@ -14,11 +140,17 @@ class ManageSchedule extends Component {
 const mapStateToProps = (state) => {
   return {
     isLoggedIn: state.user.isLoggedIn,
+    allDoctors: state.admin.allDoctors,
+    language: state.app.language,
+    allScheduleTime: state.admin.allScheduleTime,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+  return {
+    fetchAllDoctors: () => dispatch(actions.fetchAllDoctors()),
+    fetchAllScheduleTime: () => dispatch(actions.fetchAllScheduleTime()),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageSchedule);
