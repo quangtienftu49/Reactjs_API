78f4fa057f7450030b500831ea64b1584ff036d9
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-06-11 00:49:19 -0400

    add a new menu manage patient and component ManagePatient

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -60,6 +60,11 @@ export const doctorMenu = [
         name: "menu.doctor.manage-schedule",
         link: "/doctor/manage-schedule",
       },
+      {
+        //Manage patients
+        name: "menu.doctor.manage-patient",
+        link: "/doctor/manage-patient",
+      },
     ],
   },
 ];
---
+++ src/containers/System/Doctor/ManagePatient.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+import "./ManagePatient.scss";
+
+class ManagePatient extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {}
+
+  async componentDidUpdate(prevProps, prevState, snapshot) {
+    if (this.props.language !== prevProps.language) {
+    }
+  }
+
+  render() {
+    return <div>Hello from Manage Patient</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManagePatient);
