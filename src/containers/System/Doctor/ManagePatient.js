a083233d1d7762964bb82b0a030e44ad6f2af9ba
quangtienftu49 <50720010+quangtienftu49@users.noreply.github.com>
2022-06-12 00:43:35 -0400

    build managePatient page and load datePicker

--- src/containers/System/Doctor/ManagePatient.js
+++ src/containers/System/Doctor/ManagePatient.js
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { FormattedMessage } from "react-intl";
 import "./ManagePatient.scss";
+import DatePicker from "../../../components/Input/DatePicker";
 
 class ManagePatient extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      currentDate: new Date(),
+    };
   }
 
   async componentDidMount() {}
@@ -16,8 +19,41 @@ class ManagePatient extends Component {
     }
   }
 
+  handleOnChangeDatePicker = (date) => {
+    this.setState({
+      currentDate: date[0],
+    });
+  };
+
   render() {
-    return <div>Hello from Manage Patient</div>;
+    return (
+      <div className="manage-patient-container">
+        <div className="manage-patient-title">Quản lý bệnh nhân</div>
+        <div className="manage-patient-body row">
+          <div className="col-4 form-group">
+            <label>Chọn ngày khám</label>
+            <DatePicker
+              onChange={this.handleOnChangeDatePicker}
+              className="form-control"
+              value={this.state.currentDate}
+            />
+          </div>
+          <div className="col-12 manage-patient-table">
+            <table style={{ width: "100%" }}>
+              <tr>
+                <th>Company</th>
+                <th>Contact</th>
+              </tr>
+              <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
